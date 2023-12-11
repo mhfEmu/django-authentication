c4f71fa67abdad01f125501bd2c7a20c487e6550
‘iMahdi’ <hasanmehedi699@gmail.com>
2023-12-11 11:54:34 +0600

    shifted from environ to decouple

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    
     path('', include('core.urls', namespace='core')),
 ]
 
--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 
 app_name = 'core'
 urlpatterns = [
-
+	path('', views.index, name='index')
 ]
