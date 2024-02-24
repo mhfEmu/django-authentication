a4235ed00cbacaa8e309d6e9b2ea421637373f8a
‘iMahdi’ <hasanmehedi699@gmail.com>
2024-02-24 11:50:12 +0600

    checking local files updates

--- config/settings.py
+++ config/settings.py
@@ -173,8 +173,8 @@ if DEBUG is False:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = '587'
-    EMAIL_HOST_USER = 'kroze7@gmail.com'
-    EMAIL_HOST_PASSWORD = 'nnydhkgnwexyuoki'
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
     EMAIL_USE_TLS = True
     
     CSRF_COOKIE_SECURE = True
